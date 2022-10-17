7d0a5169bb759e148a5b85351f0a2961f8a9590f
zaac2240632 <zaac2240632@yahoo.com.tw>
2022-10-17 15:08:47 +0800

    first commit

---
+++ indexTEST.js
@@ -0,0 +1,180 @@
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+
+const movies = []
+let filteredMovies = []
+const MOVIES_PER_PAGE = 12
+let MovieMode = 0  //顯示模式參數
+let PageSet = 1 //當前頁數
+
+const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
+const changeMode = document.querySelector('#change-mode')
+
+
+function renderMovieList(data) {
+  let rawHTML = ''
+  data.forEach((item) => {
+    // title, image
+    rawHTML += `<div class="col-sm-3">
+    <div class="mb-2">
+      <div class="card">
+        <img src="${POSTER_URL + item.image
+      }" class="card-img-top" alt="Movie Poster">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}">More</button>
+          <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+  })
+  dataPanel.innerHTML = rawHTML
+}
+function renderMovieListByList(data) {
+  let rawHTML = ''
+  data.forEach((item) => {
+    // title, image
+    rawHTML += `
+<ul class="list-group col-sm-12 mb-2">
+  <li class="list-group-item d-flex justify-content-between">
+      <h5>${item.title}</h5>
+      <div>
+       <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}">More</button>
+          <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
+          </div>
+  </li>
+</ul>`
+  })
+  dataPanel.innerHTML = rawHTML
+}
+
+function showMovieModal(id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+  axios.get(INDEX_URL + id).then((response) => {
+    const data = response.data.results
+    modalTitle.innerText = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${POSTER_URL + data.image
+      }" alt="movie-poster" class="img-fluid">`
+  })
+}
+
+function addToFavorite(id) {
+  const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+  const movie = movies.find(movie => movie.id === id)
+
+  if (list.some(movie => movie.id === id)) {
+    return alert('此電影已經在收藏清單中!')
+  }
+
+  list.push(movie)
+  console.log(list)
+
+  localStorage.setItem('favoriteMovies', JSON.stringify(list))
+  //const jsonstring = JSON.stringify(list)  //資料轉JSON字串
+  //console.log('Json string: ', jsonstring)
+  //console.log('Josn object: ', JSON.parse(jsonstring))  //JSON字串轉資料
+}
+
+function getMoviesByPage(page) {
+  //計算起始 index 
+  const data = filteredMovies.length ? filteredMovies : movies
+  const startIndex = (page - 1) * MOVIES_PER_PAGE
+  //回傳切割後的新陣列
+  return data.slice(startIndex, startIndex + MOVIES_PER_PAGE)
+}
+
+function renderPaginator(amount) {
+  //計算總頁數
+  const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE)
+  //製作 template 
+  let rawHTML = ''
+
+  for (let page = 1; page <= numberOfPages; page++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`
+  }
+  //放回 HTML
+  paginator.innerHTML = rawHTML
+}
+
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    console.log(event.target)
+    showMovieModal(Number(event.target.dataset.id))
+  } else if (event.target.matches('.btn-add-favorite')) {
+    addToFavorite(Number(event.target.dataset.id))
+  }
+})
+
+searchForm.addEventListener('submit', function onSearchFormSubmiited(event) {
+  event.preventDefault() //沒下這筆的話，網頁會直接重整
+  const keyword = searchInput.value.trim().toLowerCase()  //toLowerCase 變成小寫
+
+
+  filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+
+  // for (const movie of movies) {
+  //   if (movie.title.toLowerCase().includes(keyword)) {
+  //     filteredMovies.push(movie)
+  //   }
+  // }
+  if (filteredMovies.length === 0) {
+    return alert('Cannot find movies with keyword: ', + keyword)
+  }
+  //重製分頁器
+  renderPaginator(filteredMovies.length)  //新增這裡
+  //預設顯示第 1 頁的搜尋結果
+  renderMovieList(filteredMovies)
+  renderMovieList(getMoviesByPage(1))  //修改這裡
+})
+
+paginator.addEventListener('click', function onPaginatorClicked(event) {
+  //如果被點擊的不是 a 標籤，結束
+  if (event.target.tagName !== 'A') return
+
+  //透過 dataset 取得被點擊的頁數
+  const page = Number(event.target.dataset.page)
+  PageSet = page
+  //更新畫面
+  if (MovieMode === 0) {
+    renderMovieList(getMoviesByPage(page))
+  } else if (MovieMode === 1) {
+    renderMovieListByList(getMoviesByPage(page))
+  }
+
+})
+//card顯示/list顯示
+changeMode.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.card-mode')) {
+    renderMovieList(getMoviesByPage(PageSet))
+    MovieMode = 0
+  } else if (event.target.matches('.list-mode')) {
+    renderMovieListByList(getMoviesByPage(PageSet))
+    MovieMode = 1
+  }
+})
+
+axios
+  .get(INDEX_URL)
+  .then((response) => {
+    movies.push(...response.data.results)
+    renderPaginator(movies.length) //新增這裡
+    renderMovieList(getMoviesByPage(1)) //修改這裡
+    //renderMovieList(movies)
+  })
+  .catch((err) => console.log(err))
+
+//localStorage.setItem('default_language', JSON.stringify)
+
